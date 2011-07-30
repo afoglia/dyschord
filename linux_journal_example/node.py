9b4a20af77b9739bcd18ce66f6928f47f4f79849
Anthony Foglia <afoglia@gmail.com>
2011-07-29 22:23:17 -0400

    Rename file and reorder if checks.
    This should be slightly faster.  (I imagine the less than comparison is just as fast as the equality, but likely to be positive more often.)

--- linux_journal_example/node.py
+++ linux_journal_example/node.py
@@ -5,9 +5,9 @@ def distance(a, b) :
   # k is the number of bits in the ids used.  For a uuid, this is 128
   # bytes.  If this were C, one would need to worry about overflow,
   # because 2**128 takes 129 bytes to store.
-  if a == b :
-    return 0
-  elif a < b:
+  if a < b:
     return b-a
+  elif a == b :
+    return 0
   else :
     return (2**k) + (b-a)
