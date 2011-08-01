6376cc51ccd1e25d79d8f1dfd8919f98ab92c606
Anthony Foglia <afoglia@gmail.com>
2011-07-31 23:42:08 -0400

    Add comment about exceptions

--- http_server_example/xmlrpclib_ServerProxy.py
+++ http_server_example/xmlrpclib_ServerProxy.py
@@ -7,3 +7,8 @@ import xmlrpclib
 
 server = xmlrpclib.ServerProxy("http://localhost:9000", verbose=True)
 print "Ping:", server.ping()
+
+# When an called method raises an exception, it gets transfered to the
+# client and raised by the client lib as an xmlrpclib.Fault exception.
+# If the connection goes down, calling will raise a socket.error
+# exception.
