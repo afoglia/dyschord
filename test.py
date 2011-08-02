bb2334493f2dfeedac72499a96117cbefea1590b
Anthony Foglia <afoglia@gmail.com>
2011-08-02 08:07:04 -0400

    Rename files.

--- test.py
+++ test.py
@@ -2,19 +2,19 @@
 
 import unittest
 
-import node
+import dyschord
 
 
 # Simple DistributedHash builder for testing
 def construct_dh(size) :
   nodes = {}
   while len(nodes) != size :
-    new_node = node.Node()
+    new_node = dyschord.Node()
     nodes[new_node.id] = new_node
-  rslt = node.DistributedHash()
+  rslt = dyschord.DistributedHash()
   for n in nodes.itervalues() :
     rslt.join(n)
-  return node.DistributedHash(n)
+  return dyschord.DistributedHash(n)
 
 # Again a function for testing, we'll build a list using the first n words from
 # the system dictionary
@@ -53,8 +53,8 @@ class SimpleTest(unittest.TestCase) :
     self.assertRaises(KeyError, self.distributed_hash.lookup, "foo")
 
   def testJoinPreExisting(self) :
-    distributed_hash = node.DistributedHash()
-    n = node.Node(1)
+    distributed_hash = dyschord.DistributedHash()
+    n = dyschord.Node(1)
     distributed_hash.join(n)
     self.assertRaises(Exception, distributed_hash.join, n)
     
@@ -68,7 +68,7 @@ class SimpleTest(unittest.TestCase) :
                    for k in self.distributed_hash.iterkeys())
     existing_nodes = set(n.id for n in self.distributed_hash._iternodes())
     while True :
-      new_node = node.Node()
+      new_node = dyschord.Node()
       if new_node.id not in existing_nodes :
         break
     self.distributed_hash.join(new_node)
@@ -83,7 +83,7 @@ class SimpleTest(unittest.TestCase) :
   # data.
       
   def testLeaveNoncontained(self) :
-    self.assertEquals(node.DistributedHash().leave(node.Node()), None)
+    self.assertEquals(dyschord.DistributedHash().leave(dyschord.Node()), None)
 
   def testLeaveNonfirst(self) :
     size = 10
@@ -109,6 +109,7 @@ class SimpleTest(unittest.TestCase) :
     self.assertEquals(self.distributed_hash.num_nodes(), old_num_nodes-1)
     for k, v in as_dict.iteritems() :
       self.assertEquals(self.distributed_hash.lookup(k), v)
+
 
 if __name__=="__main__" :
   unittest.main()
